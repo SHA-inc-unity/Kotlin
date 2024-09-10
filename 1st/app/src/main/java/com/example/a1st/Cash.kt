8f10ebd09e70015dc93eda10331fe0894f64c710
SHA-inc-unity <zzz20042013@gmail.com>
2024-09-10 11:12:36 +0300

    added 2st

--- 1st/app/src/main/java/com/example/a1st/Cash.kt
+++ 1st/app/src/main/java/com/example/a1st/Cash.kt
@@ -8,6 +8,14 @@ class Cash(
     var cashCategory: String,
     var cashData: Date) {
 
+    constructor(
+        cashId: Int,
+         cashMinus: Int,
+         cashCategory: String,
+         cashData: Date,
+         doubleDate: Date
+    ) : this(cashId, cashMinus, cashCategory, cashData)
+
     fun getCash() : String {
         return "lost: ${cashMinus}; type: ${cashCategory}; date: ${cashData}"
     }
